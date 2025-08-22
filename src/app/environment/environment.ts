77300370691429811e9e2eac0b4992d31b088a12
Yinka09 <kadirimaranatha@yahoo.com>
2025-08-22 17:02:13 +0100

    edit enviroment file

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,1 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+};
 
+export const API_KEY = 'AIzaSyC9ubkYP_WXAwPjwJs2YFNFnhRHtt2MFyg';
+export const SIGNUP_BASE_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY;
+
+export const SIGNIN_BASE_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+  API_KEY;
