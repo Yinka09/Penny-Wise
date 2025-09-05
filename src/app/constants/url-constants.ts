7491627b3a53f9b01d2fde421551f646244be117
Yinka09 <kadirimaranatha@yahoo.com>
2025-09-05 19:10:54 +0100

    Update environment configuration

--- src/app/constants/url-constants.ts
+++ src/app/constants/url-constants.ts
@@ -1,7 +1,8 @@
-import { API_KEY } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 export const SIGNUP_BASE_URL =
-  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY;
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+  environment.API_KEY;
 
 export const SIGNIN_BASE_URL =
   'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-  API_KEY;
+  environment.API_KEY;
